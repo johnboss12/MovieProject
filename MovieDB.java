423934fb2e16def24ec935de74c500e09eeee634
johnboss12 <trevor.lockwich2@d11.org>
2026-08-20 13:11:19 -0600

    Think this is working yo

--- MovieDB.java
+++ MovieDB.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class MovieDB {
+    public static ArrayList<Movie> readMoviesIntoAL(String fileName){
+       try(Scanner in = new Scanner(new File(fileName))){    
+            ArrayList<Movie> db = new ArrayList<>();
+        //enter is new movie, commas seperate PIVS
+            while (in.hasNextLine()) {
+                String[] movie = in.nextLine().split("[,]");
+                m = new Movie(movie[0],movie[1],movie[2],movie[3]);
+                db.add(m);
+            }
+       }catch (Exception e) {
+        System.out.println("Can't find the file likely");
+        System.err.println(e);
+       }
+
+       return db;
+    }
+    
+}
