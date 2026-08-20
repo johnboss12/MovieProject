5d5b0d351f8737448421db059e87521d915cc3f0
johnboss12 <trevor.lockwich2@d11.org>
2026-08-20 13:17:55 -0600

    works better if at all

--- MovieDB.java
+++ MovieDB.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MovieDB {
@@ -7,15 +9,14 @@ public class MovieDB {
         //enter is new movie, commas seperate PIVS
             while (in.hasNextLine()) {
                 String[] movie = in.nextLine().split("[,]");
-                m = new Movie(movie[0],movie[1],movie[2],movie[3]);
+                Movie m = new Movie(movie[0],movie[1],movie[2],movie[3]);
                 db.add(m);
             }
+            return db;
        }catch (Exception e) {
         System.out.println("Can't find the file likely");
         System.err.println(e);
        }
-
-       return db;
     }
     
 }
