8bcdff16db5793d8ad0429d7f1ab659d97fa35ef
Ben2996 <David.tipton@d11.org>
2026-08-20 13:22:08 -0600

    PIVs Done

--- MovieDB.java
+++ MovieDB.java
@@ -1,1 +1,25 @@
+public class MovieDB{
+    private String title;
+    private String actor;
+    private int year;
+    private String directors;
 
+    public MovieDB(String t, String a, int y, String d){
+        title = t;
+        actor = a;
+        year = y;
+        directors = d;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getActor(){
+        return actor;
+    }
+    public int getYear(){
+        return year;
+    }
+    public String getDirector(){
+        return directors;
+    }
+}
