5f43d34f8e5988111a9aafedecbbc5be056a5d3e
johnboss12 <trevor.lockwich2@d11.org>
2026-08-24 11:52:59 -0600

    Ya its all goo,man

---
+++ Movie.java
@@ -0,0 +1,25 @@
+public class Movie {
+    private String title;
+    private String actor;
+    private int year;
+    private String directors;
+
+    public Movie(String t, String a, int y, String d){
+        title = t;
+        actor = a;
+        year = y;
+        directors = d;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getActor(){
+        return actor;
+    }
+    public int getYear(){
+        return year;
+    }
+    public String getDirector(){
+        return directors;
+    }
+}
